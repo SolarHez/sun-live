be1e5d33207d016a2b12f7e93353f84cf7a0865d
SolarHez <solarhez@gmail.com>
2026-06-10 04:35:51 +0800

    - 增加视频播放器

--- src/app/(test)/video.tsx
+++ src/app/(test)/video.tsx
@@ -1,9 +1,31 @@
 import { View } from "react-native";
 import { usePlayerServers } from "@/hooks/test/usePlayerSevers";
+import { VLCPlayer, VlCPlayerView } from "react-native-vlc-media-player";
+import { StyleSheet } from "react-native";
 
 export default function VideoPlayer() {
   const { playUrl, handleGetHuyaPlayUrl, handleGetDouyuPlayUrl } =
     usePlayerServers();
 
-  return <View></View>;
+  return (
+    <View>
+      <VlCPlayerView
+        autoplay={false}
+        url="https://www.radiantmediaplayer.com/media/big-buck-bunny-360p.mp4"
+        ggUrl=""
+        showGG={true}
+        showTitle={true}
+        title="Big Buck Bunny"
+        showBack={true}
+        onLeftPress={() => {}}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  video: {
+    width: "100%",
+    height: "100%",
+  },
+});
